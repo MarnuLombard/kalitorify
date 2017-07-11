e7341c6d731a2ccbd60b9f6271971332604b269f
Marnu <marnu@mdstech.co.za>
2017-07-11 07:27:02 +0200

    Add network.sh script source and clean it up a bit

--- network.sh
+++ network.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# As per https://apple.stackexchange.com/questions/191879/how-to-find-the-currently-connected-network-service-from-the-command-line
+# Provided by Reorx
+
 services=$(networksetup -listnetworkserviceorder | grep 'Hardware Port')
 
 while read line; do
-    sname=$(echo $line | awk -F  "(, )|(: )|[)]" '{print $2}')
-    sdev=$(echo $line | awk -F  "(, )|(: )|[)]" '{print $4}')
+    sname=$(echo "$line" | awk -F  "(, )|(: )|[)]" '{print $2}')
+    sdev=$(echo "$line" | awk -F  "(, )|(: )|[)]" '{print $4}')
     #echo "Current service: $sname, $sdev, $currentservice"
     if [ -n "$sdev" ]; then
-        ifout="$(ifconfig $sdev 2>/dev/null)"
+        ifout="$(ifconfig "$sdev" 2>/dev/null)"
         echo "$ifout" | grep 'status: active' > /dev/null 2>&1
         rc="$?"
         if [ "$rc" -eq 0 ]; then
@@ -19,9 +22,9 @@ while read line; do
 done <<< "$(echo "$services")"
 
 if [ -n "$currentservice" ]; then
-    echo $currentservice
-    echo $currentdevice
-    echo $currentmac
+    echo "$currentservice"
+    echo "$currentdevice"
+    echo "$currentmac"
 else
     >&2 echo "Could not find current service"
     exit 1
